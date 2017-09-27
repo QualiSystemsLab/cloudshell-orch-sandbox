04369f91d89d08a51a668a35135503adba447df8
jimbr70 <jim.b@qualisystems.com>
2017-09-27 11:02:50 -0700

    Added a subst_log variable that is passed in case it is needed in future debugging - found it helpful but it is never writen to log -just for interactve use in debug for now

--- sandbox_scripts/QualiEnvironmentUtils/ConfigFileManager.py
+++ sandbox_scripts/QualiEnvironmentUtils/ConfigFileManager.py
@@ -24,12 +24,14 @@ class ConfigFileManager:
         """
         try:
             concrete_config_data = template_config_data
+            subst_log = ""
             # Replace {ConfigPool.PARAM} with PARAM's value from the pool
             it = re.finditer(r'\{ConfigPool\:[^}]*\}', concrete_config_data, flags=re.IGNORECASE)
             for match in it:
                 param = match.group()
                 if param.lower() in config_set_pool_data:
                     concrete_config_data = concrete_config_data.replace(param, config_set_pool_data[param.lower()])
+                    subst_log += param + ' = ' + config_set_pool_data[param.lower()] + '\n'
                 else:
                     raise Exception('Could not find attribute ' + param.lower() + ' in the config pool')
 
@@ -39,19 +41,19 @@ class ConfigFileManager:
                 param = match.group()
                 quali_note = 'Built from template: ' + strftime('%Y-%b-%d %H:%M:%S', gmtime())
                 concrete_config_data = concrete_config_data.replace(param, quali_note)
-
+                subst_log += param + ' = ' + quali_note + '\n'
             # Replace {Device.Self.Name} with the resource's name
             it = re.finditer(r'\{Device:Self:Name\}', concrete_config_data, flags=re.IGNORECASE)
             for match in it:
                 param = match.group()
                 concrete_config_data = concrete_config_data.replace(param, resource.name)
-
+                subst_log += param + ' = ' + resource.name + '\n'
             # Replace {Device.Self.Address} with the resource's management ip
             it = re.finditer(r'\{Device:Self:Address\}', concrete_config_data, flags=re.IGNORECASE)
             for match in it:
                 param = match.group()
                 concrete_config_data = concrete_config_data.replace(param, resource.address)
-
+                subst_log += param + ' = ' + resource.address + '\n'
             # Replace {Device.Self.ATTRIBUTE_NAME} with the resource's attribute value
             # TODO: Need to decode password attributes: Password, Enable Password, and SNMP Read Community
             it = re.finditer(r'\{Device:Self\:[^}]*\}', concrete_config_data, flags=re.IGNORECASE)
@@ -62,7 +64,7 @@ class ConfigFileManager:
                 param_val = resource.get_attribute(att_name)
                 # param_val = resource.get_attribute(param)
                 concrete_config_data = concrete_config_data.replace(param, param_val)
-
+                subst_log += param + ' = ' + param_val + '\n'
             # Replacement of params from types: {Device:ALIAS:Attribute_name} WHERE ALIAS is at any structure level
             it = re.finditer(r'\{Device:[^}]*\}', concrete_config_data, flags=re.IGNORECASE)
             the_resources = None
@@ -82,11 +84,12 @@ class ConfigFileManager:
                             raise Exception("Could not find attribute '{0}' in resource '{1}'".format(alias_attribname,
                                                                                                       resource.name))
                         concrete_config_data = concrete_config_data.replace(param, param_val)
+                        subst_log += param + ' = ' + param_val + '\n'
                         break
                 if concrete_name <= ' ':
                     raise Exception('Could not find a resource with alias ' + sb_alias + '; likely missing from blueprint.')
 
-            return concrete_config_data
+            return concrete_config_data, subst_log
         except Exception as ex:
             raise QualiError('ConfigFileManager', "Failed to create concrete config for " + resource.name +
                              " from template. Unexpected error: " + ex.message)
